89b1bfc706fba138a5135a4d908e8a48f48f494a
zhxing <zhxing_cas@163.com>
2017-10-12 17:12:00 +0800

    4

---
+++ 4/4.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+	//cout << 12 / 3 * 4 + 5 * 15 + 24 % 5 / 2 << endl;     //
+	//int i;
+	//if (i =42)
+	//	cout << "true" << endl;
+	//double dval;
+	//int ival;
+	//int *pi;
+	//pi=0;
+
+	//vector<int> v(10);
+	//auto vv = v.begin();
+	//while (vv != v.end())
+	//{
+	//	cout << *vv++ << " ";
+	//}
+
+	////4.12
+	//vector<int> v12(10, 1);
+	//for (auto vv12 = v12.begin(); vv12 != v12.end(); vv12++)
+	//{
+	//	if (*vv12 % 2 != 0)
+	//		*vv12 *= 2;
+	//}
+	//cout << endl;
+	//for (auto vv12 = v12.begin(); vv12 != v12.end(); vv12++)
+	//{
+	//	
+	//		cout<<*vv12<<" ";
+	//}
+	//cout << endl;
+	//cout << 'q'<<endl;
+	//cout << (~'q' << 6) << endl;
+
+	
+
+	return 0;
+}
